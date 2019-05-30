36bb108bb169b21c07576618afa2a36f09e06962
Federico Armellini <armellini.federico@gmail.com>
2019-05-30 23:22:12 +0200

    v 0.2 con generatore

--- ProgettoApi2019/Program.cs
+++ ProgettoApi2019/Program.cs
@@ -8,16 +8,37 @@ namespace ProgettoApi2019
     {
         // a è amico di b
 
-                        //amico_di          //b       //lista di a
+                                //amico_di          //b       //lista di a
         public static Dictionary<string, Dictionary<string, List<string> >> relazioni;
+
+        public static Random rnd;
 
         static void Main(string[] args)
         {
+            //args = new string[] { "-g", "100", "150000"};
+
+            if (args.Length>0)
+            {
+                if (args[0] == "-g")
+                {
+                    Generatore(args);
+                    return;
+                }
+            }
+
+            Solve("input.txt");
+            Console.Write(Report());
+            Console.ReadKey();
+        }
+
+        private static void Solve(string v)
+        {
+
             relazioni = new Dictionary<string, Dictionary<string, List<string>>>();
             string[] s = null;
             try
             {
-                s = File.ReadAllLines("input.txt");
+                s = File.ReadAllLines(v);
             }
             catch
             {
@@ -26,7 +47,7 @@ namespace ProgettoApi2019
                 return;
             }
 
-            foreach(string s2 in s)
+            foreach (string s2 in s)
             {
                 if (s2.StartsWith("addrel"))
                 {
@@ -50,15 +71,149 @@ namespace ProgettoApi2019
                 }
             }
 
-            Report();
-            Console.ReadKey();
         }
 
-        private static void Report()
+        private static void Generatore(string[] args)
         {
+            //=> syntax    .exe -g [NUM_TEST] [AVARAGE_LINES_PER_TEST]
+            //=> example   .exe -g 100 150
+
+
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Need more arguments");
+                Console.ReadKey();
+                return;
+            }
+
+            rnd = new Random();
+            CreaDirectory();
+
+            int n_test = Convert.ToInt32(args[1]);
+            int avg_lines = Convert.ToInt32(args[2]);
+            for(int i=0; i< n_test; i++)
+            {
+                GeneraTest(avg_lines);
+            }
+
+            Console.WriteLine(n_test + " generated.");
+            Console.ReadKey();
+            return;
+
+        }
+
+        private static void CreaDirectory()
+        {
+            ProvaCreaDirectory("o");
+            ProvaCreaDirectory("i");
+        }
+
+        private static void ProvaCreaDirectory(string v)
+        {
+            if (Directory.Exists(v))
+            {
+                return;
+            }
+
+            try
+            {
+                Directory.CreateDirectory(v);
+            }
+            catch
+            {
+                ;
+            }
+        }
+
+        private static void GeneraTest(int avg_lines)
+        {
+            List<string> L = new List<string>();
+            for (int i=0; i<avg_lines; i++)
+            {
+                L.Add(GeneraLinea());
+            }
+
+            string output_path = GetOutputPath();
+            File.WriteAllLines("i/" + output_path, L);
+            Solve("i/" + output_path);
+            Save("o/" + output_path);
+        }
+
+        private static void Save(string v)
+        {
+            string output_content = Report();
+            File.WriteAllText(v, output_content);
+        }
+
+        private static string GetOutputPath()
+        {
+            return GetDataOra() + ".txt";
+        }
+
+        private static string GetDataOra()
+        {
+            return DateTime.Now.Year + "_" + DateTime.Now.Month.ToString().PadLeft(2, '0') + "_" + DateTime.Now.Day.ToString().PadLeft(2, '0') + "_" + DateTime.Now.Hour.ToString().PadLeft(2, '0') + "_" + DateTime.Now.Minute.ToString().PadLeft(2, '0') + "_" + DateTime.Now.Second.ToString().PadLeft(2, '0') + DateTime.Now.Millisecond.ToString().PadLeft(3, '0');
+        }
+
+        private static string GeneraLinea()
+        {
+            int choice = rnd.Next(0, 100);
+            if (choice >= 0 && choice <= 25)
+                return GeneraAddEnt();
+            else if (choice > 25 && choice <= 50)
+                return GeneraDelEnt();
+            else if (choice > 50 && choice <= 85)
+                return GeneraAddRel();
+            else if (choice > 85 && choice <= 100)
+                return GeneraDelRel();
+            else
+                throw new Exception();
+        }
+
+        private static string GeneraDelRel()
+        {
+            char carattere1 = RandomLettera();
+            char carattere2 = RandomLettera();
+            char carattere3 = RandomLettera();
+            return "delrel \"" + carattere1 + "\" \"" + carattere2 + "\" \"" + carattere3 + "\"";
+        }
+
+        private static string GeneraAddRel()
+        {
+            char carattere1 = RandomLettera();
+            char carattere2 = RandomLettera();
+            char carattere3 = RandomLettera();
+            return "addrel \"" + carattere1 + "\" \"" + carattere2 + "\" \"" + carattere3 + "\"";
+        }
+
+        private static char RandomLettera()
+        {
+            int lettera = rnd.Next(0, 26);
+            lettera += 'a';
+            char carattere = (char)lettera;
+            return carattere;
+        }
+
+        private static string GeneraDelEnt()
+        {
+            char carattere = RandomLettera();
+            return "delent \"" + carattere + "\"";
+        }
+
+        private static string GeneraAddEnt()
+        {
+            int lettera = rnd.Next(0, 26);
+            lettera += 'a';
+            char carattere = (char)lettera;
+            return "addent \""+carattere+"\"";
+        }
+
+        private static string Report()
+        {
+            string output = "";
             foreach (var x in relazioni.Keys)
             {
-                Console.Write(x);
+
 
                 List<string> champions = new List<string>();
                 int max = -1;
@@ -77,15 +232,21 @@ namespace ProgettoApi2019
                     }
                 }
 
-                champions.Sort();
-
-                foreach(var x2 in champions)
+                if (max > 0)
                 {
-                    Console.Write(" " + x2);
-                }
+                    champions.Sort();
 
-                Console.Write(" " + max + ";" + '\n');
+                    output += x;
+                    foreach (var x2 in champions)
+                    {
+                        output += (" " + x2);
+                    }
+
+                    output += (" " + max + ";" + '\n');
+                }
             }
+
+            return output;
         }
 
         private static void DelRel(string s2)
